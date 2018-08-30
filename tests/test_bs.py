58384861af0a4d2648eb23ee9b89414fdee7044e
Alex Waite <alex@waite.eu>
2018-08-30 11:52:37 +0200

    add tests for login(); remove bs tests

--- tests/test_bs.py
+++
@@ -1,7 +0,0 @@
-from pigskin.pigskin import pigskin
-
-"""Just some BS tests to get the ball rolling"""
-
-def test_agent():
-    gp = pigskin()
-    assert gp.user_agent == 'Firefox'
---
+++ tests/test_login.py
@@ -0,0 +1,17 @@
+import os
+import logging
+import pytest
+
+from pigskin.pigskin import pigskin
+
+@pytest.mark.vcr()
+def test_login_success():
+    gp = pigskin()
+    result = gp.login(username=pytest.gp_username, password=pytest.gp_password)
+    assert result == True
+
+@pytest.mark.vcr()
+def test_login_failure():
+    gp = pigskin()
+    result = gp.login(username='I_do_not_exist', password='wrong')
+    assert result == False
