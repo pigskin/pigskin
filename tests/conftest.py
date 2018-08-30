29323bd4b4c500295dd6d1db452787bdeb19b28c
Alex Waite <alex@waite.eu>
2018-08-30 10:49:33 +0200

    scrub credentials before saving vcr cassettes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,37 @@
 import os
 import pytest
+try:
+    from urllib import quote  # Python 2.X
+except ImportError:
+    from urllib.parse import quote  # Python 3+
+
+from pigskin.pigskin import pigskin
+
+
+pytest.gp_username = os.getenv('PIGSKIN_USER', '')
+pytest.gp_password = os.getenv('PIGSKIN_PASS', '')
+
+scrub_list = [
+    pytest.gp_username,
+    pytest.gp_password,
+    quote(pytest.gp_username),
+    quote(pytest.gp_password)
+]
+
+def scrub_request(request):
+    for i in scrub_list:
+        request.body = request.body.decode().replace(i, 'REDACTED').encode()
+    return request
+
+def scrub_response(response):
+    for i in scrub_list:
+        response['body']['string'] = response['body']['string'].replace(i, 'REDACTED')
+    return response
 
 @pytest.fixture
 def vcr_config():
     return {
-        'decode_compressed_response': True
+        'decode_compressed_response': True,
+        'before_record_request': scrub_request,
+        #'before_record_response': scrub_response
     }
