eaa22f0e3a75440bed860a75e9b47bba28eb7920
Alex Waite <alex@waite.eu>
2018-08-31 15:22:11 +0200

    add tests for refresh_tokens()

---
+++ tests/test_refresh_tokens.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+
+from pigskin.pigskin import pigskin
+
+@pytest.mark.vcr()
+def test_refresh_tokens():
+    gp = pigskin()
+
+    # login
+    login_success = gp.login(username=pytest.gp_username, password=pytest.gp_password)
+    assert login_success == True
+
+    # make sure tokens are actually set
+    assert gp.access_token != ''
+    assert gp.refresh_token != ''
+
+    # store the initial tokens
+    first_access_token = gp.access_token
+    first_refresh_token = gp.refresh_token
+
+    # refresh the tokens
+    refresh_success = gp.refresh_tokens()
+    assert refresh_success == True
+
+    # make sure new tokens are actually set
+    assert gp.access_token != ''
+    assert gp.refresh_token != ''
+
+    # and finally make sure they've actually been refreshed
+    assert first_access_token != gp.access_token
+    assert first_refresh_token != gp.refresh_token
