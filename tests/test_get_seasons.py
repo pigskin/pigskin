459dea18be4446c4bc296e2896d380d22bf505c4
Alex Waite <alex@waite.eu>
2018-09-01 15:19:10 +0000

    add get_seasons() with tests
    One step towards retiring get_seasons_and_weeks()

---
+++ tests/test_get_seasons.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+
+from pigskin.pigskin import pigskin
+
+@pytest.mark.vcr()
+def test_get_seasons():
+    gp = pigskin()
+    gp.login(username=pytest.gp_username, password=pytest.gp_password)
+
+    seasons = gp.get_seasons()
+
+    # make sure we have a response
+    assert seasons
+
+    # make sure it's sorted high to low
+    assert seasons == sorted(seasons, reverse=True)
+
+    # make sure the seasons look sane-ish
+    for i in seasons:
+        assert int(i) > 2000 and int(i) < 2050
+
+
+@pytest.mark.vcr()
+def test_get_seasons_failure():
+    gp = pigskin()
+    gp.login(username=pytest.gp_username, password=pytest.gp_password)
+
+    # change to a wrong URL
+    gp.config['modules']['ROUTES_DATA_PROVIDERS']['games'] = 'https://httpbin.org/json'
+    seasons = gp.get_seasons()
+
+    # make sure we have a response
+    assert not seasons
+
+    # another wrong URL
+    gp.config['modules']['ROUTES_DATA_PROVIDERS']['games'] = 'https://httpbin.org/html'
+    seasons = gp.get_seasons()
+
+    # make sure we have a response
+    assert not seasons
