8f65b8ae2bc68aa9e53198c96379c9bad05ae855
Alex Waite <alex@waite.eu>
2018-09-01 07:05:29 +0000

    add another test for refresh_tokens(); small cleanup

--- tests/test_refresh_tokens.py
+++ tests/test_refresh_tokens.py
@@ -12,8 +12,8 @@ def test_refresh_tokens():
     assert login_success == True
 
     # make sure tokens are actually set
-    assert gp.access_token != ''
-    assert gp.refresh_token != ''
+    assert gp.access_token
+    assert gp.refresh_token
 
     # store the initial tokens
     first_access_token = gp.access_token
@@ -24,9 +24,21 @@ def test_refresh_tokens():
     assert refresh_success == True
 
     # make sure new tokens are actually set
-    assert gp.access_token != ''
-    assert gp.refresh_token != ''
+    assert gp.access_token
+    assert gp.refresh_token
 
     # and finally make sure they've actually been refreshed
     assert first_access_token != gp.access_token
     assert first_refresh_token != gp.refresh_token
+
+@pytest.mark.vcr()
+def test_refresh_tokens_no_login():
+    gp = pigskin()
+
+    # refresh the tokens
+    refresh_success = gp.refresh_tokens()
+    assert refresh_success == False
+
+    # make sure new tokens are not set
+    assert not gp.access_token
+    assert not gp.refresh_token
