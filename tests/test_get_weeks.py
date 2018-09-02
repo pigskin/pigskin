1b321a51edb6c2df8e8df009aba4a48e25dae298
Alex Waite <alex@waite.eu>
2018-09-02 05:15:03 +0000

    test passing both str and int to get_weeks(season)

--- tests/test_get_weeks.py
+++ tests/test_get_weeks.py
@@ -8,22 +8,24 @@ def test_get_weeks():
     gp = pigskin()
     gp.login(username=pytest.gp_username, password=pytest.gp_password)
 
-    weeks = gp.get_weeks(2017)
+    weeks_int = gp.get_weeks(2017)
+    weeks_str = gp.get_weeks('2017')
 
-    # make sure we have a response
-    assert weeks
-    # and all the season types
-    assert weeks['pre']
-    assert weeks['reg']
-    assert weeks['post']
+    for weeks in [weeks_int, weeks_str]:
+        # make sure we have a response
+        assert weeks
+        # and all the season types
+        assert weeks['pre']
+        assert weeks['reg']
+        assert weeks['post']
 
-    # make sure the week numbers look sane
-    for week in weeks['pre']:
-        assert int(week) >= 0 and int(week) <= 4
-    for week in weeks['reg']:
-        assert int(week) >= 1 and int(week) <= 17
-    for week in weeks['post']:
-        assert int(week) >= 18 and int(week) <= 22
+        # make sure the week numbers look sane
+        for week in weeks['pre']:
+            assert int(week) >= 0 and int(week) <= 4
+        for week in weeks['reg']:
+            assert int(week) >= 1 and int(week) <= 17
+        for week in weeks['post']:
+            assert int(week) >= 18 and int(week) <= 22
 
 
 @pytest.mark.vcr()
