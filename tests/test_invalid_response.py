9eeeb566faa90fde1ac3cc2e772000f32d67e984
Alex Waite <alex@waite.eu>
2018-09-04 12:41:52 +0200

    add xml response to invalid response tests

--- tests/test_invalid_response.py
+++ tests/test_invalid_response.py
@@ -7,8 +7,8 @@ gp = pigskin()
 
 
 @pytest.mark.vcr()
-def test_invalid_response_get_json():
-    junk_url = 'https://httpbin.org/json'
+def test_invalid_response_get_bytes():
+    junk_url = 'https://httpbin.org/bytes/20'
     gp.config['modules']['API'] = { key: junk_url for key in gp.config['modules']['API'] }
     gp.config['modules']['ROUTES_DATA_PROVIDERS'] = { key: junk_url for key in gp.config['modules']['ROUTES_DATA_PROVIDERS'] }
 
@@ -43,8 +43,26 @@ def test_invalid_response_get_html():
 
 
 @pytest.mark.vcr()
-def test_invalid_response_get_bytes():
-    junk_url = 'https://httpbin.org/bytes/20'
+def test_invalid_response_get_json():
+    junk_url = 'https://httpbin.org/json'
+    gp.config['modules']['API'] = { key: junk_url for key in gp.config['modules']['API'] }
+    gp.config['modules']['ROUTES_DATA_PROVIDERS'] = { key: junk_url for key in gp.config['modules']['ROUTES_DATA_PROVIDERS'] }
+
+    assert not gp.get_current_season_and_week()
+    assert not gp.get_seasons()
+    assert not gp.get_weeks(2017)
+    assert not gp.get_games(2017, 'reg', 8)
+    assert not gp.get_team_games('2018', '49ers')
+    assert not gp.get_game_versions('2017090700', '2017')
+    assert not gp.get_nfl_network_streams()
+    assert not gp.get_redzone_streams()
+    assert not gp._get_diva_config(junk_url)
+    assert not gp._get_diva_streams(video_id='invalid', diva_config_url=junk_url)
+
+
+@pytest.mark.vcr()
+def test_invalid_response_get_xml():
+    junk_url = 'https://httpbin.org/xml'
     gp.config['modules']['API'] = { key: junk_url for key in gp.config['modules']['API'] }
     gp.config['modules']['ROUTES_DATA_PROVIDERS'] = { key: junk_url for key in gp.config['modules']['ROUTES_DATA_PROVIDERS'] }
 
@@ -66,7 +84,7 @@ def test_invalid_response_post_json():
     gp.config['modules']['API'] = { key: junk_url for key in gp.config['modules']['API'] }
     gp.config['modules']['ROUTES_DATA_PROVIDERS'] = { key: junk_url for key in gp.config['modules']['ROUTES_DATA_PROVIDERS'] }
 
-    # these POST, and as far as I can tell, httbin only provides JSON responses to POST
-    # TODO: find a way to test these for html and bytes as well
+    # it seems that httbin only provides JSON responses to POST requests
+    # TODO: find a way to get html and bytes responses for POST as well
     assert not gp.login(username='nope', password='so_secret')
     assert not gp.refresh_tokens()
