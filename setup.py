5bd4af1c8c91746e33c91b4ca62db07d7b83f77e
Alex Waite <alex@waite.eu>
2018-09-11 15:03:10 +0200

    greatly simplify setup.py
    It just seemed... silly and complicated. I instead followed info straight from
    the horse's mouth:
    https://packaging.python.org/tutorials/packaging-projects/
    https://setuptools.readthedocs.io/en/latest/setuptools.html

--- setup.py
+++ setup.py
@@ -1,125 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
+import setuptools
 
-import os
-import sys
-from shutil import rmtree
+#
+# Grab some info from supporting files
+#
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
-from setuptools import find_packages, setup, Command
+from pigskin.__version__ import VERSION
+__version__ = '.'.join(map(str, VERSION))
 
-# Package meta-data.
-NAME = 'pigskin'
-DESCRIPTION = 'A Python library for NFL Game Pass.'
-LONG_DESCRIPTION = DESCRIPTION
-URL = 'https://github.com/aqw/pigskin'
-EMAIL = 'alex@waite.eu'
-AUTHOR = 'Alex Waite and many others'
-REQUIRES_PYTHON = ">2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*"
-
-# What packages are required for this module to be executed?
-INSTALL_DEPS = [
-    'requests',
-    'm3u8',
-]
-TEST_DEPS = [
-    'pyflakes',
-    'pytest',
-    'pytest-cov',
-    'pytest-vcr',
-    'vcrpy',
-]
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
-    exec(f.read(), about)
-    about['__version__'] = '.'.join(map(str, about['VERSION']))
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=INSTALL_DEPS,
-    extras_require=EXTRAS,
-    tests_require=TEST_DEPS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
+#
+# Provide a little info about who we are
+#
+setuptools.setup(
+    name = 'pigskin',
+    version = __version__,
+    author = 'Alex Waite and many others',
+    author_email = 'alex@waite.eu',
+    description = 'A Python library for NFL Game Pass',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/aqw/pigskin',
+    license = 'MIT',
+    classifiers = [
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    python_requires = ">2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    packages = setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires = [
+        'requests',
+        'm3u8',
+    ],
+    tests_require = [
+        'pyflakes',
+        'pytest',
+        'pytest-cov',
+        'pytest-vcr',
+        'vcrpy',
+    ],
 )
