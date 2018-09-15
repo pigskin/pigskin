394bdfed6340f2bcf537f3fa873b0c86553bf7b2
Alex Waite <alex@waite.eu>
2018-09-15 11:46:11 +0200

    move authentication code to europe/auth.py

---
+++ pigskin/europe/auth.py
@@ -0,0 +1,217 @@
+import logging
+import requests
+from .. import settings
+
+class auth(object):
+    def __init__(self, store):
+        self._store = store
+        self.logger = logging.getLogger(__name__)
+
+
+    def check_for_subscription(self):
+        """Check if the user has a valid subscription."""
+        url = self._store.gp_config['modules']['API']['USER_ACCOUNT']
+        headers = {'Authorization': 'Bearer {0}'.format(self._store.access_token)}
+
+        try:
+            r = self._store.s.get(url, headers=headers)
+            #self._log_request(r)
+            data = r.json()
+        except ValueError:
+            self.logger.error('check_for_subscription: unable to parse server response')
+            return False
+
+        try:
+            assert data['subscriptions']
+        except (KeyError, AssertionError):
+            self.logger.error('No active NFL Game Pass Europe subscription was found.')
+            return False
+
+        return True
+
+
+    def login(self, username, password, force=False):
+        """Login to NFL Game Pass Europe."""
+        # if the user already has access, just skip the entire auth process
+        if not force:
+            if self.check_for_subscription():
+                self.logger.debug('No need to login; the user already has access.')
+                return True
+
+
+        for auth in [self._gp_auth, self._gigya_auth]:
+            self.logger.debug('Trying {0} authentication.'.format(auth.__name__))
+            try:
+                data = auth(username, password)
+                assert data['access_token']
+                assert data['refresh_token']
+            except (KeyError, AssertionError):
+                self.logger.error('Could not acquire GP tokens')
+            except Exception as e:
+                raise e
+            else:
+                self._store.username = username
+                # TODO: are these tokens provided for valid accounts without a subscription?
+                self._store.access_token = data['access_token']
+                self._store.refresh_token = data['refresh_token']
+
+                self.logger.debug('login was successful')
+                return True
+
+        self.logger.error('login failed')
+        return False
+
+
+    def refresh_tokens(self):
+        """Refresh the tokens needed to access content."""
+        url = self._store.gp_config['modules']['API']['REFRESH_TOKEN']
+        post_data = {
+            'client_id': self._store.gp_config['modules']['API']['CLIENT_ID'],
+            'refresh_token': self._store.refresh_token,
+            'grant_type': 'refresh_token'
+        }
+
+        try:
+            r = self._store.s.post(url, data=post_data)
+            #self._log_request(r)
+            data = r.json()
+        except ValueError:
+            self.logger.error('token refresh: server response is invalid')
+            return False
+        except Exception as e:
+            raise e
+
+        try:
+            self._store.access_token = data['access_token']
+            self._store.refresh_token = data['refresh_token']
+        except KeyError:
+            self.logger.error('could not find GP tokens to refresh')
+            return False
+        except Exception as e:
+            raise e
+
+        # TODO: check for status codes, just in case
+
+        self.logger.debug('successfully refreshed tokens')
+        return True
+
+
+    def _gigya_auth(self, username, password):
+        """Authenticate to Game Pass by first going through Gigya's
+        authentication servers.
+
+        Parameters
+        ----------
+        username : str
+            Your NFL Game Pass username.
+        password : str
+            The user's password.
+
+        Returns
+        -------
+        dict
+            A dict containing the authentication data; empty if there's an error
+
+        See Also
+        --------
+        ``login()``
+        ``_gp_auth()``
+        """
+        url = settings.gigya_auth_url
+        api_key = self._store.gp_config['modules']['GIGYA']['JAVASCRIPT_API_URL'].split('apiKey=')[1]
+        post_data = {
+            'apiKey' : api_key,
+            'loginID' : username,
+            'password' : password
+        }
+
+        try:
+            r = self._store.s.post(url, data=post_data)
+            #self._log_request(r)
+            gigya_data = r.json()
+        except ValueError:
+            self.logger.error('_gigya_auth: server response is invalid')
+            return {}
+        except Exception as e:
+            raise e
+
+        try:
+            # make sure some key data is here
+            assert gigya_data['UID']
+            assert gigya_data['UIDSignature']
+            assert gigya_data['signatureTimestamp']
+        except AssertionError:
+            pass
+        except KeyError:
+            self.logger.error('could not parse gigya auth response')
+            return {}
+        except Exception as e:
+            raise e
+
+        data = self._gp_auth(username, password, gigya_data)
+
+        return data
+
+
+    def _gp_auth(self, username, password, gigya_data=False):
+        """Authenticate to the Game Pass servers.
+
+        Parameters
+        ----------
+        gigya : dict
+            The data needed to authenticate using gigya auth data.
+
+        Returns
+        -------
+        dict
+            A dict containing the authentication data; empty if there's an error
+
+        See Also
+        --------
+        ``_gigya_auth()``
+        ``login()``
+        """
+        url = self._store.gp_config['modules']['API']['LOGIN']
+
+        post_data = {
+            'client_id': self._store.gp_config['modules']['API']['CLIENT_ID'],
+            'username': username,
+            'password': password,
+            'grant_type': 'password'
+        }
+
+        if gigya_data:
+            # TODO: audit if in fact all these fields are needed
+            post_data = {
+                'client_id' : self._store.gp_config['modules']['API']['CLIENT_ID'],
+                'uuid' : gigya_data['UID'],
+                'signature' : gigya_data['UIDSignature'],
+                'ts' : gigya_data['signatureTimestamp'],
+                'errorCode' : '0',
+                'username' : username,
+                'password' : password,
+                'grant_type' : 'password'
+            }
+
+        try:
+            r = self._store.s.post(url, data=post_data)
+            #self._log_request(r)
+            data = r.json()
+        except ValueError:
+            self.logger.error('_gp_auth: server response is invalid')
+            return {}
+        except Exception as e:
+            raise e
+
+        try:
+            # make sure some key data is here
+            assert data['access_token']
+            assert data['refresh_token']
+        except (KeyError, AssertionError):
+            self.logger.error('could not parse auth response')
+            return {}
+        except Exception as e:
+            raise e
+
+        # TODO: check for status codes, just in case
+        return data
--- pigskin/pigskin.py
+++ pigskin/pigskin.py
@@ -21,6 +21,7 @@ import requests
 import m3u8
 
 from . import settings
+from .europe.auth import auth
 from .europe.data import data
 
 
@@ -53,6 +54,7 @@ class pigskin(object):
         self.nfln_shows = {}
         self.episode_list = []
 
+        self._auth = auth(self._store)
         self._data = data(self._store)
 
         self.logger.debug('Python Version: %s' % sys.version)
@@ -214,127 +216,6 @@ class pigskin(object):
         return response
 
 
-    def _gigya_auth(self, username, password):
-        """Authenticate to Game Pass by first going through Gigya's
-        authentication servers.
-
-        Parameters
-        ----------
-        username : str
-            Your NFL Game Pass username.
-        password : str
-            The user's password.
-
-        Returns
-        -------
-        dict
-            A dict containing the authentication data; empty if there's an error
-
-        See Also
-        --------
-        ``login()``
-        ``_gp_auth()``
-        """
-        url = settings.gigya_auth_url
-        api_key = self._store.gp_config['modules']['GIGYA']['JAVASCRIPT_API_URL'].split('apiKey=')[1]
-        post_data = {
-            'apiKey' : api_key,
-            'loginID' : username,
-            'password' : password
-        }
-
-        try:
-            r = self._store.s.post(url, data=post_data)
-            self._log_request(r)
-            gigya_data = r.json()
-        except ValueError:
-            self.logger.error('_gigya_auth: server response is invalid')
-            return {}
-        except Exception as e:
-            raise e
-
-        try:
-            # make sure some key data is here
-            assert gigya_data['UID']
-            assert gigya_data['UIDSignature']
-            assert gigya_data['signatureTimestamp']
-        except AssertionError:
-            pass
-        except KeyError:
-            self.logger.error('could not parse gigya auth response')
-            return {}
-        except Exception as e:
-            raise e
-
-        data = self._gp_auth(username, password, gigya_data)
-
-        return data
-
-
-    def _gp_auth(self, username, password, gigya_data=False):
-        """Authenticate to the Game Pass servers.
-
-        Parameters
-        ----------
-        gigya : dict
-            The data needed to authenticate using gigya auth data.
-
-        Returns
-        -------
-        dict
-            A dict containing the authentication data; empty if there's an error
-
-        See Also
-        --------
-        ``_gigya_auth()``
-        ``login()``
-        """
-        url = self._store.gp_config['modules']['API']['LOGIN']
-
-        post_data = {
-            'client_id': self._store.gp_config['modules']['API']['CLIENT_ID'],
-            'username': username,
-            'password': password,
-            'grant_type': 'password'
-        }
-
-        if gigya_data:
-            # TODO: audit if in fact all these fields are needed
-            post_data = {
-                'client_id' : self._store.gp_config['modules']['API']['CLIENT_ID'],
-                'uuid' : gigya_data['UID'],
-                'signature' : gigya_data['UIDSignature'],
-                'ts' : gigya_data['signatureTimestamp'],
-                'errorCode' : '0',
-                'username' : username,
-                'password' : password,
-                'grant_type' : 'password'
-            }
-
-        try:
-            r = self._store.s.post(url, data=post_data)
-            self._log_request(r)
-            data = r.json()
-        except ValueError:
-            self.logger.error('_gp_auth: server response is invalid')
-            return {}
-        except Exception as e:
-            raise e
-
-        try:
-            # make sure some key data is here
-            assert data['access_token']
-            assert data['refresh_token']
-        except (KeyError, AssertionError):
-            self.logger.error('could not parse auth response')
-            return {}
-        except Exception as e:
-            raise e
-
-        # TODO: check for status codes, just in case
-        return data
-
-
     def login(self, username, password, force=False):
         """Login to NFL Game Pass.
 
@@ -363,34 +244,7 @@ class pigskin(object):
         --------
         ``check_for_subscription()``
         """
-        # if the user already has access, just skip the entire auth process
-        if not force:
-            if self.check_for_subscription():
-                self.logger.debug('No need to login; the user already has access.')
-                return True
-
-
-        for auth in [self._gp_auth, self._gigya_auth]:
-            self.logger.debug('Trying {0} authentication.'.format(auth.__name__))
-            try:
-                data = auth(username, password)
-                assert data['access_token']
-                assert data['refresh_token']
-            except (KeyError, AssertionError):
-                self.logger.error('Could not acquire GP tokens')
-            except Exception as e:
-                raise e
-            else:
-                self._store.username = username
-                # TODO: are these tokens provided for valid accounts without a subscription?
-                self._store.access_token = data['access_token']
-                self._store.refresh_token = data['refresh_token']
-
-                self.logger.debug('login was successful')
-                return True
-
-        self.logger.error('login failed')
-        return False
+        return self._auth.login(username, password, force)
 
 
     def check_for_subscription(self):
@@ -410,24 +264,7 @@ class pigskin(object):
         --------
         ``login()``
         """
-        url = self._store.gp_config['modules']['API']['USER_ACCOUNT']
-        headers = {'Authorization': 'Bearer {0}'.format(self._store.access_token)}
-
-        try:
-            r = self._store.s.get(url, headers=headers)
-            self._log_request(r)
-            data = r.json()
-        except ValueError:
-            self.logger.error('check_for_subscription: unable to parse server response')
-            return False
-
-        try:
-            assert data['subscriptions']
-        except (KeyError, AssertionError):
-            self.logger.error('No active NFL Game Pass Europe subscription was found.')
-            return False
-
-        return True
+        return self._auth.check_for_subscription()
 
 
     def refresh_tokens(self):
@@ -440,38 +277,10 @@ class pigskin(object):
 
         Note
         ----
-        TODO: raise an error on failure so people can catch and attempt to login again
+        TODO: Exception should be raised on failure so people can catch and
+              attempt to login again
         """
-        url = self._store.gp_config['modules']['API']['REFRESH_TOKEN']
-        post_data = {
-            'client_id': self._store.gp_config['modules']['API']['CLIENT_ID'],
-            'refresh_token': self._store.refresh_token,
-            'grant_type': 'refresh_token'
-        }
-
-        try:
-            r = self._store.s.post(url, data=post_data)
-            self._log_request(r)
-            data = r.json()
-        except ValueError:
-            self.logger.error('token refresh: server response is invalid')
-            return False
-        except Exception as e:
-            raise e
-
-        try:
-            self._store.access_token = data['access_token']
-            self._store.refresh_token = data['refresh_token']
-        except KeyError:
-            self.logger.error('could not find GP tokens to refresh')
-            return False
-        except Exception as e:
-            raise e
-
-        # TODO: check for status codes, just in case
-
-        self.logger.debug('successfully refreshed tokens')
-        return True
+        return self._auth.refresh_tokens()
 
 
     def get_current_season_and_week(self):
